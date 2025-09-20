2af99f3443d24c3e7f470407241f73d8e702e1c7
tota-kn <nonbiriactive@gmail.com>
2025-09-20 21:50:10 +0900

    update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { includeIgnoreFile } from "@eslint/compat"
 import eslint from "@eslint/js"
 import prettierConfig from "eslint-config-prettier"
+import { defineConfig } from "eslint/config"
 import path from "node:path"
 import { fileURLToPath } from "node:url"
 import tseslint from "typescript-eslint"
@@ -9,7 +10,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 const gitignorePath = path.resolve(__dirname, ".gitignore")
 
-export default tseslint.config(
+export default defineConfig(
   eslint.configs.recommended,
   tseslint.configs.recommended,
   includeIgnoreFile(gitignorePath),
